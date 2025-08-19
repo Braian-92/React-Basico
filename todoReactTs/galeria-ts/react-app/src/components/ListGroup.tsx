9e1ab63bf1eef40fc4d3bd7c34a1849600c9988b
Braian-92 <zamudiobraianhernan@gmail.com>
2025-08-18 22:08:09 -0300

    eventos sobre los elementos

--- todoReactTs/galeria-ts/react-app/src/components/ListGroup.tsx
+++ todoReactTs/galeria-ts/react-app/src/components/ListGroup.tsx
@@ -1,11 +1,34 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items.map(item => <li className="list-group-item">{item}</li>);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // items = [];
+
+  const message = items.length === 0 ? <p>No items found</p> : null;
+
+  if (items.length === 0) {
+    return (
+      <>
+        <h1>ListGroup</h1>
+        <p>No items found</p>
+      </>
+    );
+  }
+
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  }
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items found</p> : null;
+  }
+  
   return ( 
     <>
       <h1>ListGroup</h1>
+      {getMessage()}
       <ul className="list-group">
-        {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+        {items.map((item, index) => <li className="list-group-item active" key={item} onClick={handleClick}>{item}</li>)}
       </ul>
     </>
   );
