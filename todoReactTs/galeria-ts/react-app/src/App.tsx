1292c982ec917a8de5875aa37c16ef698ca5e3ad
Braian-92 <zamudiobraianhernan@gmail.com>
2025-08-18 22:52:21 -0300

    base alertas

--- todoReactTs/galeria-ts/react-app/src/App.tsx
+++ todoReactTs/galeria-ts/react-app/src/App.tsx
@@ -1,4 +1,5 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App(){
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -8,7 +9,8 @@ function App(){
     console.log(item);
   }
 
-  return <div>
+  return <div className="container my-2">
+    <Alert />
     <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
     <ListGroup items={items2} heading="Cities 2" onSelectItem={handleSelectItem} />
   </div>;
